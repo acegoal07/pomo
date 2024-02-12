c53c68c942439471a767a12189bbb26cef1b8302
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2024-02-12 16:51:36 +0000

    Chnages to everything

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -9,44 +9,96 @@ let timer;
 const pomodoros = 0;
 
 function timer(time) {
-    timer = time;
-    setTimeout(() => {
-        timer--;
-        if (timer > 0) {
-            timer(timer);
-        }
-    }, 1000);
+  timer = time;
+  setTimeout(() => {
+      timer--;
+      if (timer > 0) {
+          timer(timer);
+      }
+  }, 1000);
 }
 
 function startPomodoro() {
-    timer(work);
-    timer(shortBreak);
-    timer(work);
-    timer(shortBreak);
-    timer(work);
-    timer(shortBreak);
-    timer(work);
-    timer(longBreak);
-    pomodoros++;
+  timer(work);
+  timer(shortBreak);
+  timer(work);
+  timer(shortBreak);
+  timer(work);
+  timer(shortBreak);
+  timer(work);
+  timer(longBreak);
+  pomodoros++;
 }
 
 //testing timer
 console.log(timer(shortBreak));
 
-exports.timer_class = class {
-    constructor() {
-        this.workLengthMS;
-        this.breakLengthMS;
-        this.currentPositionMS;
-    }
 
-    startTimer() {
-        this.currentPositionMS = this.workLengthMS;
-        this.timer = setInterval(() => {
-            this.currentPositionMS -= 1000;
-            if (this.currentPositionMS <= 0) {
-                clearInterval(this.timer);
-            }
-        }, 1000);
+
+
+
+
+exports.timer_class = class {
+  constructor() {
+    this.workLengthMS;
+    this.currentWorkPositionMS;
+    this.breakLengthMS;
+    this.currentBreakPositionMS;
+  }
+
+  /**
+   * @param {Boolean} isDefault
+   * @param {int} customInput
+   * @returns {exports.timer_class}
+   */
+  setWorkTimer(isDefault == true, customInput) {
+    if (isDefault) {
+      this.workLengthMS = 25 * 60 * 1000;
+    } else {
+      this.workLengthMS = customInput * 60 * 1000;
     }
+    return this;
+  }
+
+  /**
+   * @param {Boolean} isDefault
+   * @param {int} customInput
+   * @returns {exports.timer_class}
+   */
+  setBreakTimer(isDefault == true, customInput) {
+    if (isDefault) {
+      this.BreakeLengthMS = 5 * 60 * 1000;
+    } else {
+      this.breakLenghtMS = customInput * 60 * 1000;
+    }
+    return this;
+  }
+
+  /**
+   * returns {int}
+   */
+  getCurrentPositionMS() {
+    return this.currentPositionMS;
+  }
+
+  /**
+   * returns {int}
+   */
+  getCurrentPosition() {
+    return (this.currentPositionMS / 1000) * 60;
+  }
+
+  /**
+   * @param {int} ms
+   */
+  setCurrentWorkPositionMS(ms) {
+    this.currentPositionMS = ms;
+  }
+
+  /**
+   * @param {int} ms
+   */
+  setCurrentBreakPositionMS(ms) {
+    this.currentBreakPositionMS = ms;
+  }
 }
