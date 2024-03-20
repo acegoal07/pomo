4f5fdf3fb223a53ad5e5d5dec91d24ca7fe81240
Dara Corrigan <148446121+DaraCorrigan@users.noreply.github.com>
2024-03-20 01:42:48 +0000

    DARA AND ALEX ARE THE BEST, WE FIXED IT, YOU'RE ALL WELCOME
    In the tumultuous world of software development, where lines of code reign supreme and every commit holds the weight of progress, there existed a system—a grand, intricate tapestry of digital architecture, meticulously crafted and nurtured by a team of brilliant minds, and Alan. This system was the heartbeat of their operations, the cornerstone upon which their dreams were built. But as with any creation of human endeavor, it was not impervious to the whims of fate.
    
    One fateful day, in the middle of the night, a seemingly routine update, disaster struck. A conflict—a cataclysmic collision of changes—erupted within the sacred repository of their codebase. Lines clashed, files tangled, and chaos reigned supreme. The system, once a paragon of stability, now lay fractured and broken, its once seamless functionality reduced to a labyrinth of errors and inconsistencies.
    
    Panic swept through the team like wildfire, for they knew the stakes were high. Deadlines loomed ominously on the horizon, and the very future of their project hung in the balance. But amidst the chaos, two figures emerged—Dara and Alex, the legendary duo known throughout the company as the Dream Team.
    
    With steely determination and unwavering resolve, Dara and Alex dove headfirst into the fray, their minds ablaze with the fervor of battle. They turned to Abigail but to no luck she turned them away just as the inn-keeper turned away Jesus. Bet she’ll regret that. Hours turned into days, days into nights, as they toiled tirelessly to unravel the tangled web of conflicts that threatened to consume their masterpiece. They pored over lines of code with the intensity of scholars deciphering ancient manuscripts, their expertise shining like beacons in the darkness.
    
    But the road to redemption was fraught with peril, for the challenges they faced were as formidable as they were numerous. They encountered merge conflicts so complex they seemed insurmountable, bugs so elusive they seemed to vanish into thin air. Yet, through sheer grit and determination, they persevered, inching ever closer to victory with each passing hour.
    
    And then, against all odds, it happened—the breakthrough they had been praying for, the elusive moment of clarity that would pave the way to salvation. With a triumphant cry, they emerged from the depths of despair, their hands trembling with the weight of their achievement. The system, once broken and battered, now stood tall and proud, its flaws eradicated and its glory restored.
    
    The team rejoiced, their spirits lifted by the miraculous feat of their saviors. For Dara and Alex had not only saved the day—they had transcended it, ascending to the ranks of legends in the annals of software development history. Their names would be whispered in reverence for generations to come, their tale a testament to the indomitable power of human ingenuity and teamwork.
    
    And so, as the sun set on another day in the tumultuous world of software development, the team stood united once more, their bonds forged stronger than ever by the crucible of adversity. For in the end, it was not the conflicts they faced that defined them, but the courage and determination with which they overcame them. And they lived happily ever after, their dreams of success and prosperity realized in the glow of their hard-won victory.

---
+++ assets/js/base.js
@@ -0,0 +1,267 @@
+// Timer class caller
+const timer = new PomoTimer();
+// Focus listener
+const docTitle = document.title;
+window.onblur = function () {
+   timer.setBlurred(true);
+};
+window.onfocus = function () {
+   timer.setBlurred(false);
+   document.title = docTitle;
+};
+// Disable drag and drop
+window.ondragstart = function () {
+   return false;
+}
+window.ondrop = function () {
+   return false;
+}
+// Onload handler
+window.addEventListener('load', () => {
+   /////////////// Onload changes ///////////////
+   if (!getNotificationPermission() && !isIOS()) {
+      Notification.requestPermission();
+   }
+   setTimerColor("var(--background-color)");
+   document.querySelector("#timer-circle-progress").classList.add("timer-circle-progress-transition");
+
+   /////////////// Universal Popup functions ///////////////
+   // Popup open listener
+   document.querySelectorAll("[data-popup-open-target]").forEach((element) => {
+      element.addEventListener('click', () => {
+         const popup = document.querySelector(`#${element.getAttribute("data-popup-open-target")}`);
+         popup.style.animation = "popupOpenAnimation 0.5s forwards";
+         popup.style.display = "flex";
+      });
+   });
+   // Popup close listener
+   document.querySelectorAll("[data-popup-close-target]").forEach((element) => {
+      element.addEventListener('click', () => {
+         const popup = document.querySelector(`#${element.getAttribute("data-popup-close-target")}`);
+         popup.style.animation = "popupCloseAnimation 0.5s forwards";
+         setTimeout(function () {
+            popup.style.display = "none";
+         }, 500);
+      });
+   });
+
+   /////////////// Todo popup functions ///////////////
+   // Add todo button listener
+   document.querySelector("#addTodoButton").addEventListener("click", (event) => {
+      event.preventDefault();
+      const todoInput = document.querySelector("#todo-input");
+      const todoText = todoInput.value;
+
+      if (todoText.trim() !== "") {
+         const todoItemContainer = document.createElement("div");
+         todoItemContainer.classList.add("todo-item-container");
+
+         const todoTextElement = document.createElement("div");
+         todoTextElement.classList.add("todo-text");
+         todoTextElement.textContent = todoText;
+
+         todoItemContainer.appendChild(todoTextElement);
+
+         const todoItem = document.createElement("div");
+         todoItem.setAttribute("data-popup-open-target", "todo-item-popup");
+         todoItem.classList.add("todo-item");
+         todoItem.appendChild(todoItemContainer);
+
+         document.querySelector(".todo-list-container").appendChild(todoItem);
+
+         todoInput.value = "";
+
+         const popup = document.querySelector("#todo-popup");
+         popup.style.animation = "popupCloseAnimation 0.5s forwards";
+         setTimeout(function () {
+            popup.style.display = "none";
+         }, 500);
+      }
+   });
+
+
+   /////////////// Leaderboard popup functions ///////////////
+   // Leaderboard switch button
+   document.querySelector("#leaderboard-switch-button").addEventListener('click', () => {
+      if (document.querySelector("#leaderboard-all-time").classList.contains("hide")) {
+         document.querySelector("#leaderboard-all-time").classList.remove("hide");
+         document.querySelector("#leaderboard-weekly").classList.add("hide");
+      } else {
+         document.querySelector("#leaderboard-all-time").classList.add("hide");
+         document.querySelector("#leaderboard-weekly").classList.remove("hide");
+      }
+   });
+
+   /////////////// Login popup functions ///////////////
+   // Login page switch button
+   document.querySelector("#go-to-registration").addEventListener('click', () => {
+      // Remove all elements from the page
+      document.querySelector("#registration-page").classList.remove("hide");
+      document.querySelector("#login-page").classList.add("hide");
+   });
+   // Login submit button
+   document.querySelector("#login-form").addEventListener('submit', (event) => {
+      event.preventDefault();
+      event.target.reset();
+      const popup = document.querySelector("#login-popup");
+      popup.style.animation = "popupCloseAnimation 0.5s forwards";
+      setTimeout(function () {
+         popup.style.display = "none";
+      }, 500);
+   });
+   // Registration page switch button
+   document.querySelector("#go-to-login").addEventListener('click', () => {
+      // Remove all elements from the page
+      document.querySelector("#registration-page").classList.add("hide");
+      document.querySelector("#login-page").classList.remove("hide");
+   });
+   // Registration submit button
+   document.querySelector("#registration-form").addEventListener('submit', (event) => {
+      event.preventDefault();
+      event.target.reset();
+      const popup = document.querySelector("#login-popup");
+      popup.style.animation = "popupCloseAnimation 0.5s forwards";
+      setTimeout(function () {
+         popup.style.display = "none";
+      }, 500);
+   });
+
+   /////////////// Timer Buttons ///////////////
+   // Start timer button
+   let pomodoros = 0;
+   let index = 0;
+   const times = [25, 5, 25, 5, 25, 5, 25, 15];
+   let currentTime;
+   let pomoProgress = 0;
+   document.querySelector("#timer-start-button").addEventListener('click', () => {
+      if (!timer.isActive()) {
+         if (timer.getCurrentPositionMS() === 0) {
+            setTimerColor("var(--accent-color)");
+            currentTime = times[index] * 1000; // Needs to be switched back to 60000 when testing is finished
+            timer.setTimerLength(currentTime)
+               .startTimer();
+         } else {
+            timer.startTimer();
+         }
+         const halfWay = timer.timerLengthMS / 2;
+         const quarterWay = timer.timerLengthMS / 4;
+         const timeDisplay = setInterval(() => {
+            if (timer.getCurrentPositionMS() === -1000) {
+               clearInterval(timeDisplay);
+               timer.stopTimer();
+               timer.setCurrentPositionMS(0);
+               if (getNotificationPermission() && document.hasFocus() === false) {
+                  const notification = new Notification("Pomo Timer", {
+                     title: "Pomo Timer",
+                     body: `${times[index] === 25 ? "Its time for your break comeback and start the timer" : "Your break has finished comeback!"}`,
+                     lang: "en-GB",
+                     icon: "assets/images/favi.webp"
+                  });
+                  notification.onclick = function () {
+                     window.focus();
+                     notification.close();
+                  };
+                  notification.onshow = function () {
+                     setTimeout(() => {
+                        notification.close();
+                     }, 5000);
+                  };
+                  notification.onerror = function (error) {
+                     console.log("Notification error: " + error);
+                  }
+               }
+               if (index < 7) {
+                  index++;
+                  pomoProgress = pomoProgress + 12.5;
+                  setPomoCounterProgress(pomoProgress);
+               } else {
+                  index = 0;
+                  pomodoros++;
+                  pomoProgress = 0;
+                  document.querySelector('#pomodoro-counter').textContent = pomodoros;
+               }
+            } else if (timer.getCurrentPositionMS() < quarterWay) {
+               setTimerColor("var(--background-color)");
+            } else if (timer.getCurrentPositionMS() < halfWay) {
+               setTimerColor("orange");
+            } else {
+               void (0);
+            }
+         }, 1000);
+      }
+   });
+   // Pause timer button
+   document.querySelector("#timer-pause-button").addEventListener('click', () => {
+      timer.stopTimer();
+   });
+});
+
+/////////////// Additional functions ///////////////
+/**
+ * Timer count down function
+ * @param {int} value
+ */
+function setTimerProgress(value) {
+   document.querySelector("#timer-circle-progress").setAttribute.bind(document.querySelector("#timer-circle-progress"))("stroke-dasharray", `${(value * 283).toFixed(0)} 283`);
+}
+const pomodoroCounterCircle = document.querySelector('#counter-circle');
+const pomodoroCounterRadius = pomodoroCounterCircle.r.baseVal.value;
+const pomodoroCounterCircumference = pomodoroCounterRadius * 2 * Math.PI;
+pomodoroCounterCircle.style.strokeDasharray = `${pomodoroCounterCircumference} ${pomodoroCounterCircumference}`;
+pomodoroCounterCircle.style.strokeDashoffset = `${pomodoroCounterCircumference}`;
+/**
+ * Pomo counter progress function
+ * @param {Number} percent
+ */
+function setPomoCounterProgress(percent) {
+   pomodoroCounterCircle.style.strokeDashoffset = pomodoroCounterCircumference - percent / 100 * pomodoroCounterCircumference;
+}
+/**
+ * Timer colour function
+ * @param {String} input
+ */
+function setTimerColor(input) {
+   document.querySelector("#timer-circle-progress").style.stroke = input == null ? "green" : input;
+}
+/**
+ * Milliseconds to timestamp
+ * @param {Number} s
+ * @returns {String}
+ */
+function msToTime(s) {
+   function pad(n, z) {
+      z = z || 2;
+      return ('00' + n).slice(-z);
+   }
+
+   const ms = s % 1000;
+   s = (s - ms) / 1000;
+   const secs = s % 60;
+   s = (s - secs) / 60;
+   const mins = s % 60;
+
+   return `${pad(mins)}:${pad(secs)}`;
+}
+/**
+ * Notification permission getter
+ * @returns {Boolean}
+ */
+function getNotificationPermission() {
+   if (isIOS()) {
+      return false;
+   }
+   return Notification.permission === "granted";
+}
+/**
+ * Check if the user is using an iOS device
+ * @returns {Boolean}
+ */
+function isIOS() {
+   const browserInfo = navigator.userAgent.toLowerCase();
+
+   if (/iphone/.exec(browserInfo) || /ipad/.exec(browserInfo)) {
+      return true;
+   }
+
+   return !!(['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platforms));
+}
