529a828a73954fbdb41ee306723e584bcd62f746
abbie <abbiereid222@gmail.com>
2024-04-16 21:05:07 +0100

    Databse connection and all the php works yay

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,23 +1,19 @@
-window.addEventListener('load', () => {
-   document.querySelector('#login-form').addEventListener('submit', async (event) => {
-      event.preventDefault();
+window.addEventListener('load', function() {
+   const loginForm = document.querySelector('#login-form');
 
-      await fetch('assets/php/login.php', {
-         method: 'POST',
-         body: new FormData(document.forms["login-form"])
-      }).then(response => {
-         console.log(response);
-         return response.text();
-      }).then(data => {
-         console.log(data);
-      }).catch(error => {
-         console.log(error);
-      });
-      const popup = document.querySelector("#login-popup");
-      popup.style.animation = "popupCloseAnimation 0.5s forwards";
-      setTimeout(function () {
-         popup.style.display = "none";
-      }, 500);
-      event.target.reset();
-   });
+   loginForm.addEventListener('submit', evt => authentication(evt));
 });
+
+async function authentication(evt) {
+   evt.preventDefault();
+
+   const formData = new FormData(evt.target);
+
+   await fetch('assets/php/login.php', {
+      method: 'POST',
+      body: formData
+   }).then(response => response.json())
+   .then(data => {
+      console.log(data);
+   });
+}
--- assets/php/login.php
+++ assets/php/login.php
@@ -1,27 +1,25 @@
 <?php
+    include 'index.php';
 
-var_dump($_POST);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-// require_once 'index.php';
+    $stmt = $conn->prepare("SELECT * FROM users WHERE userName = ? AND Password = ?");
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
 
-// $username = $_POST['username'];
-// $password = $_POST['password'];
+    $result = $stmt->get_result();
 
-// $sql = "SELECT * FROM `users` WHERE `username` = ? AND `password` = ?";
-// $stmt = $conn->prepare($sql);
-// $stmt->bind_param("ss", $username, $password);
-// $stmt->execute();
-// $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+        $response['success'] = true;
+    } else {
+        $response['success'] = false;
+        $response['message'] = "This user does not exist, or the password you have entered is incorrect";
+    }
 
-// if ($result->num_rows > 0) {
-//   $response['success'] = true;
-// } else {
-//   $response['success'] = false;
-//   $response['message'] = "This user does not exist, or the password you have entered is incorrect";
-// }
+    $stmt->close();
+    $conn->close();
 
-// $stmt->close();
-// $conn->close();
-
-// header('Content-Type: application/json');
-// echo json_encode($response);
+    header('Content-Type: application/json');
+    echo json_encode($response);
+?>
